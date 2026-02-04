b99b59470ceaceb2d41d7288630124d005a5612f
Diego Rodríguez <91118595+juanrobyte@users.noreply.github.com>
2026-02-04 14:39:12 -0400

    vite edit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,       // equivale a 0.0.0.0
-    port: 54127,      // puerto poco común
-    strictPort: true,
-  },
+  base: "/portfolio/",
 });
