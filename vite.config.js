25269765a7703a05abad9bc50dfb5a33af92b9e6
Diego Rodríguez <91118595+juanrobyte@users.noreply.github.com>
2026-02-10 18:28:50 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: "/portfolio/",  // porque tu repo se llama "portfolio"
+  build: {
+    outDir: "docs",     // 👈 GitHub Pages sí permite /docs
+    emptyOutDir: true,
+  },
 });
